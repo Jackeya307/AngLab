0f2662cae74c585443b00b5d98be969e8534252e
jackeya307 <a30786531@gmail.com>
2024-08-17 22:47:12 +0800

    回家作業練習

--- src/app/login-practice/login-practice.component.ts
+++ src/app/login-practice/login-practice.component.ts
@@ -3,29 +3,82 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-practice',
   templateUrl: './login-practice.component.html',
-  styleUrls: ['./login-practice.component.css']
+  styleUrls: ['./login-practice.component.css'],
 })
 export class LoginPracticeComponent {
-  title = '新增'
+  txtContext = {
+    title: '新增',
+    accLabel: '帳號: ',
+    pwLabel: '密碼: ',
+    submit: '送出',
+  };
 
-  acc = ''
-  pw = ''
+  userData = {
+    acc: '',
+    pw: '',
+  };
 
-  submit = '送出'
-  change = '切換'
+  error = {
+    acc:'請輸入帳號',
+    pw:'請輸入密碼',
+    accExists: '此帳號已存在',
+    pwExists: '此密碼已存在',
+  }
 
-  accDis = false
-  spanHidden = true
-  submitInfo(e:any){
-    if(!(this.acc&&this.pw)){
-      alert('請填入資訊')
-      return
-    }
-    this.spanHidden = !this.spanHidden
+  userArray = [
+    {
+      acc: '001',
+      pw: '1234',
+    },
+  ];
+
+  change = '切換';
+
+  accDis = false;
+  tbody = false;
+
+  errState = {
+    pwError: false,
+    accError: false,
+    accExistsError: false,
+    pwExistsError: false,
 
   }
-  switch(){
-    this.title = this.title ==='修改'? '新增': '修改'
-    this.accDis = !this.accDis
+  submitInfo() {
+    this.errState.accError = false
+    this.errState.pwError = false
+    this.errState.accExistsError = false
+    this.errState.pwExistsError = false
+
+    if (!this.userData.acc) {
+      this.errState.accError = true
+      return;
+    }
+
+    if(!this.userData.pw){
+      this.errState.pwError = true
+      return
+    }
+    let sd = this.userArray.includes(this.userData)
+    this.userArray.forEach(item => {
+      if( item.acc === this.userData.acc){
+        this.errState.accExistsError = true
+        return
+      }
+    });
+
+
+
+    this.userArray.push({...this.userData});
+
+
+    this.clearInputFields();
+  }
+  switch() {
+    this.txtContext.title = this.txtContext.title === '修改' ? '新增' : '修改';
+    this.accDis = !this.accDis;
+  }
+  clearInputFields() {
+    this.userData = { acc: '', pw: '' };
   }
 }
